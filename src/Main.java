8a5bd0277b74f7785f4a317fd0a32d015b121ab5
RedStarZ77 <redstarz2002@mail.ru>
2025-04-06 23:25:45 +0300

    Stream lambda practice

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        Director director = new Director();
-        String result = director.doWork((int n) -> {
-            for (int i = 0; i < n; i++) {
-                System.out.println("Working");
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            numbers.add(new Random().nextInt(1000));
+        }
+        List<Integer> filtered = filter(numbers, (n) -> n % 2 == 0);
+        System.out.println(filtered);
+    }
+
+    private static List<Integer> filter(List<Integer> list, Predicate predicate) {
+        List<Integer> result = new ArrayList<>();
+        for (int i : list) {
+            if (predicate.test(i)) {
+                result.add(i);
             }
-            return "Success";
-        }, 5);
-        System.out.println(result);
+        }
+        return result;
     }
 }
